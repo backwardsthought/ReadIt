b9318dc0862c92881a2e6039c0794d879bb5daf9
Felipe Lobo <felipe.lobo@involves.com.br>
2018-09-28 19:36:33 -0300

    Add Pocket credentials

---
+++ ReadIt/Pocket+Credentials.swift
@@ -0,0 +1,20 @@
+//
+// Created by Felipe Lobo on 2018-09-28.
+// Copyright (c) 2018 Copyisright. All rights reserved.
+//
+
+extension Pocket {
+
+	static var host: String {
+		return "https://getpocket.com"
+	}
+
+	static var redirectUri: String {
+		return "pocketapp79730:authorizationFinished"
+	}
+
+	static var consumerKey: String {
+		return ""
+	}
+
+}
