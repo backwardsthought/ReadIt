b6ea66a817a28557bc4597a0b16f12d5dcdfe87c
Bruno da Luz <brunolabx@gmail.com>
2019-11-29 00:27:28 -0300

    Remove dynamicSystem

--- ReadIt/Extensions/UIColor+Extensions.swift
+++ ReadIt/Extensions/UIColor+Extensions.swift
@@ -1,19 +1,7 @@
 import UIKit
 
 extension UIColor {
-	
-	static var dynamicSystem: UIColor = {
-		if #available(iOS 13.0, *) {
-			return UIColor { (trait: UITraitCollection) -> UIColor in
-				if trait.userInterfaceStyle == .light {
-					return .white
-				}
-				return .black
-			}
-		}
-		return .white
-	}()
-	
+
 	static var invertedColors: UIColor = {
 		if #available(iOS 13.0, *) {
 			return UIColor { (trait: UITraitCollection) -> UIColor in
